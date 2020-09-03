74f14200e90b2e3fb0fd5f19869006dfd94951cf
endpoint020 <endpoint020@163.com>
2020-09-03 14:54:55 +0800

    Update byte.py
    add url

--- byte.py
+++ byte.py
@@ -1,5 +1,5 @@
 from ctypes import *
 mylib = CDLL('/mnt/hgfs/qrcreate/libadd.so')
-
+#https://www.yanxurui.cc/posts/python/2017-06-18-3-ways-of-calling-c-functions-from-python/
 add = mylib.add
 print add(1,2)
